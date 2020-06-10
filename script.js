48e674fc09b8e66d38562691973124441c4f7023
Mufida Salma <46271228+upida@users.noreply.github.com>
2020-06-10 14:19:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,47 @@
 var number = 0;
-$('body').ready(function(){
-  $(document).ready(function () {
-    $('.arrow').click(function () {
-      $('.title').css('font-size', '30px');
-      $('.title').css('top', '5%');
-      $('.title').css('left', '5.5%');
-      $('.title').css('transition-duration', '2s');
-      $('.content').css('margin-top', '3%');
-      $('.content').css('margin-left', '15%');
-      $('#content').fadeOut('500', function () {
-        $('#content').load('outdoor.html');
-        $('#content').fadeIn('slow', function () {
-          $('.menu').css('display', 'block');
-          $('.title').html('<a href="/">IQBAL</a>');
-          count(number);
-        });
-      });
-    });
-    $('#outdoor').click(function () {
-      number = 0;
-      $('#content').fadeOut('500', function () {
-        $('#content').load('outdoor.html');
-        $('#content').fadeIn('slow', function () {
-          count(number);
-        });
-      });
-    });
-    $('#school').click(function () {
-      number = 1;
-      $('#content').fadeOut('500', function () {
-        $('#content').load('school.html');
-        $('#content').fadeIn('slow', function () {
-          count(number);
-        });
-      });
-    });
-    $('#me').click(function () {
-      number = 2;
-
-      $('#content').fadeOut('500', function () {
-        $('#content').load('me.html');
-        $('#content').fadeIn('slow', function () {
-          count(number);
-        });
+$(document).ready(function () {
+  $('.arrow').click(function () {
+    $('.title').css('font-size', '30px');
+    $('.title').css('top', '5%');
+    $('.title').css('left', '5.5%');
+    $('.title').css('transition-duration', '2s');
+    $('.content').css('margin-top', '3%');
+    $('.content').css('margin-left', '15%');
+    $('#content').fadeOut('500', function () {
+      $('#content').load('outdoor.html');
+      $('#content').fadeIn('slow', function () {
+        $('.menu').css('display', 'block');
+        $('.title').html('<a href="/">IQBAL</a>');
+        count(number);
       });
     });
   });
+  $('#outdoor').click(function () {
+    number = 0;
+    $('#content').fadeOut('500', function () {
+      $('#content').load('outdoor.html');
+      $('#content').fadeIn('slow', function () {
+        count(number);
+      });
+    });
+  });
+  $('#school').click(function () {
+    number = 1;
+    $('#content').fadeOut('500', function () {
+      $('#content').load('school.html');
+      $('#content').fadeIn('slow', function () {
+        count(number);
+      });
+    });
+  });
+  $('#me').click(function () {
+    number = 2;
 
+    $('#content').fadeOut('500', function () {
+      $('#content').load('me.html');
+      $('#content').fadeIn('slow', function () {
+        count(number);
+      });
+    });
+  });
 });
